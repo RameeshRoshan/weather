bca735ec8f316db0a8ef2bb85d38315f0b2f87eb
rameesh roshan <rameeshroshan.roshan@gmail.com>
2021-07-28 15:19:03 +0530

    final

--- weather/src/WeatherTile.js
+++ weather/src/WeatherTile.js
@@ -8,30 +8,48 @@ export default class WeatherTile extends Component {
             <div className="tilediv" key={idx}>
                 <button id="close" onClick={()=>deletecity(idx)} >X</button>
                 <div>
-                    <h2>{city.location.name}</h2>
-                    <p>{city.location.region}</p>
-                    <p>{city.location.tz_id}</p>
+                    <h2 className="name">{city.location.name}</h2>
+                    <p className="smallfont">{city.location.region}</p>
+                    <p className="smallfont">{city.location.tz_id}</p>
                 </div>
                 <div className="row_flex">
                     <div>
-                        <img src={city.current.condition.icon} alt="icon"/>
-                        <p id="condition">{city.current.condition.text}</p>
+                        <img id="icon" src={city.current.condition.icon} alt="icon"/>
                     </div>
                     <div>
                         <h1 id="temp">{city.current.temp_c}<span className="small">&#x2103;</span></h1>
+                    </div>
+                </div>
+                <div className="row_flex">
+                    <div>
+                        <p id="condition">{city.current.condition.text}</p>
+                    </div>
+                    <div>
                         <p>Feels Like: <span>{city.current.feelslike_c}</span></p>
                     </div>
                 </div>
                 <div className="row_flex">
                     <div className="left">
-                        <p>Pressure:  <span>{city.current.pressure_mb}</span>mb</p>
-                        <p>Wind: <span>{city.current.wind_kph}</span>kph</p>
-                        <p>Precipitation: <span>{city.current.precip_mm}</span>mm</p>
+                        <div className="row">
+                            <div className="smallfont">Pressure: </div> <div>{city.current.pressure_mb} mb</div>
+                        </div>
+                        <div className="row">
+                            <div className="smallfont">Wind: </div> <div>{city.current.wind_kph}kph</div>
+                        </div>
+                        <div className="row">
+                            <div className="smallfont">Precipitation: </div><div>{city.current.precip_mm}mm</div>
+                        </div>
                     </div>
-                    <div className="left">
-                        <p>Visibility: <span>{city.current.vis_km}</span>km</p>
-                        <p>Humidity: <span>{city.current.humidity}</span>%</p>
-                        <p>UV index: <span>{city.current.uv}</span></p>
+                    <div className="right">
+                        <div className="row">
+                            <div className="smallfont">Visibility: </div><div>{city.current.vis_km}km</div>
+                        </div>
+                        <div className="row">
+                            <div className="smallfont">Humidity: </div><div> {city.current.humidity}%</div>
+                        </div>
+                        <div className="row">
+                            <div className="smallfont">UV index: </div><div>{city.current.uv}</div>
+                        </div>
                     </div>
                 </div>
             </div>
